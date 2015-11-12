f75f93643b5a6ef7ea8d9782761a94d87c5eff1c
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-11-12 16:24:15 +0300

    *Задание 1 * Используя Visual Studio, создайте проект по шаблону Console Application. Напишите программу, в которой метод будет вызываться рекурсивною. Каждый новый вызов метода выполняется в отдельном потоке.

---
+++ Mentoring/Mentoring/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Mentoring
+{
+    using System;
+    using System.Threading;
+
+    internal class Program
+    {
+        public static void Beta()
+        {
+            Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " started.");
+            Thread.Sleep(200);
+            var oThread = new Thread(Beta);
+            oThread.Start();
+            Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " run subthread.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " finished.");
+        }
+
+        public static int Main()
+        {
+            Console.WriteLine("Thread Start/Stop/Join Sample");
+
+            var oThread = new Thread(Beta);
+            oThread.Start();
+
+            Console.WriteLine("Program finished");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
