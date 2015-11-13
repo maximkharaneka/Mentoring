d9e6aef413b2544f6d236921660e54022cd72d78
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-11-13 03:42:41 +0300

    *Задание 2 * Создайте приложение, которое может быть запущено только в одном экземпляре (используя именованный Mutex).

---
+++ Mentoring/03.2_Synchronization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _03._2_Synchronization
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var createdNew = true;
+            using (var mutex = new Mutex(true, "Synchronization", out createdNew))
+            {
+                if (createdNew)
+                {
+                    Console.WriteLine("Runned Successfuly");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Already runned");
+                    // Console.ReadLine();
+                }
+            }
+        }
+    }
+}
