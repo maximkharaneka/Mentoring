447f2a92f2cbe573ee295053eb9a43ea5d1e9f14
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-11-20 03:44:32 +0300

    Задание 2 Создайте консольное приложение, в котором организуйте асинхронный вызов метода. Используя конструкцию BeginInvoke передайте в поток некоторую информацию (возможно, в формате строки). Организуйте обработку переданных данных в callback методе.

---
+++ Mentoring/04.2_Asynchronous/Async.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace _04._2_Asynchronous
+{
+    public class Async
+    {
+        public string TestMethod(int callDuration, string input, out string threadId)
+        {
+            Console.WriteLine("Test method begins.");
+            Thread.Sleep(callDuration);
+            threadId = input + Thread.CurrentThread.ManagedThreadId;
+            return string.Format("My call time was {0}.", callDuration);
+        }
+    }
+}
---
+++ Mentoring/04.2_Asynchronous/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace _04._2_Asynchronous
+{
+    internal class Program
+    {
+        public delegate string AsyncMethodCaller(int callDuration, string input, out string threadId);
+
+        private static void Main()
+        {
+            var ad = new Async();
+
+            AsyncMethodCaller caller = ad.TestMethod;
+
+            var dummy = "Main+";
+
+            var result = caller.BeginInvoke(3000,
+                "Main+",
+                out dummy,
+                CallbackMethod,
+                "The call executed on thread {0}, with return value \"{1}\".");
+
+            Console.WriteLine("The main thread {0} continues to execute...",
+                Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(4000);
+
+            Console.WriteLine("The main thread ends.");
+            Console.ReadLine();
+        }
+
+
+        private static void CallbackMethod(IAsyncResult ar)
+        {
+            var result = (AsyncResult) ar;
+            var caller = (AsyncMethodCaller) result.AsyncDelegate;
+
+            var formatString = (string) ar.AsyncState;
+
+            var threadId = string.Empty;
+
+            var returnValue = caller.EndInvoke(out threadId, ar);
+
+            Console.WriteLine(formatString, threadId, returnValue);
+        }
+    }
+}
