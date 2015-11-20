c58f403d01563444c662edd2249d17c06278358f
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-11-20 03:53:29 +0300

    Задание 1 Создайте массив чисел размерностью в 1 000 000 или более. Используя генератор случайных чисел, проинициализируйте этот массив значениями. Создайте PLINQ запрос, который позволит получить все нечетные числа из исходного массива.

---
+++ Mentoring/05.1_TPL/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._1_TPL
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rand = new Random();
+            var numbers = new int[1000000];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(1000000);
+            }
+
+            var output = numbers.AsParallel().Where(x => x%2 == 0).ToList();
+
+            output.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
