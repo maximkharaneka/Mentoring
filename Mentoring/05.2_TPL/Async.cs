457c266b814780739f2e42b607c67fccde0dbaa8
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-11-20 04:14:08 +0300

    Задание 2 Создайте два метода, которые будут выполняться в рамках параллельных задач. Организуйте вызов этих методов при помощи Invoke таким образом, чтобы основной поток программы (метод Main) не остановил свое выполнение.

---
+++ Mentoring/05.2_TPL/Async.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace _05._2_TPL
+{
+    public class Async
+    {
+        public string TestMethod(int callDuration, string input, out string threadId)
+        {
+            Console.WriteLine("Test method "+input+" begins.");
+            Thread.Sleep(callDuration);
+            threadId = input + Thread.CurrentThread.ManagedThreadId;
+            return string.Format("My call " + input + " time was {0}.", callDuration);
+        }
+    }
+}
---
+++ Mentoring/05.2_TPL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _05._2_TPL
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ad = new Async();
+            Task task = new Task(() => Parallel.Invoke(
+                () =>
+                {
+                    Console.WriteLine("Begin first task...");
+                    string output;
+                    ad.TestMethod(2000, "First", out output);
+                },
+                () =>
+                {
+                    Console.WriteLine("Begin second task...");
+                    string output;
+                    ad.TestMethod(1000, "Second", out output);
+                }
+                ));
+
+            task.Start();
+           
+            Console.WriteLine("Main thread finished");
+            task.Wait();
+            Console.WriteLine("Tasks waited");
+            Console.ReadLine();
+        }
+    }
+}
