5a7951f2c20bfd747c1e05974897e249e7d9b5eb
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-11-20 04:51:35 +0300

    Задание 1 Создайте приложение WindowsForms. На главной форме приложения разместите 3 кнопки с названиями: IsComplete, End, Callback. Организуйте обработчики нажатия на кнопки таким образом, чтобы они инициировали асинхронное выполнение некоторого метода (метод определите сами, можно воспользоваться чем-то вроде Add или более абстрактного Compute). Для каждой из кнопок завершение асинхронного метода должно отслеживаться соответствующим образом: • · IsComplete – с использованием значения свойства IsComplete • · End – просто применяя EndInvoke • · Callback – с использованием callback метода

--- Mentoring/04.1_Asynchronous/Form1.cs
+++ Mentoring/04.1_Asynchronous/Form1.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +8,66 @@ namespace _04._1_Asynchronous
 {
     public partial class Form1 : Form
     {
+        private delegate void AsyncMethodCaller(int callDuration);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void AsyncMethod(int callDuration)
+        {
+            MessageBox.Show("Started.");
+            Thread.Sleep(callDuration);
+            MessageBox.Show("Finished.");
+        }
+
+        private static void CallbackMethod(IAsyncResult ar)
+        {
+            MessageBox.Show("Callback started.");
+            AsyncResult result = (AsyncResult)ar;
+            AsyncMethodCaller caller = (AsyncMethodCaller)result.AsyncDelegate;
+            
+            string formatString = (string)ar.AsyncState;
+            
+            int threadId = 0;
+
+            MessageBox.Show("Callback waiting started.");
+            caller.EndInvoke(ar);
+            MessageBox.Show("Callback finished.");
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var task = new Task(() =>AsyncMethod(2000));
+
+            MessageBox.Show("Task created.");
+            task.Start();
+            MessageBox.Show("Task started.");
+            while (task.IsCompleted);
+            MessageBox.Show("Task completed.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var caller = new AsyncMethodCaller(AsyncMethod);
+            MessageBox.Show("Task Created.");
+            IAsyncResult result = caller.BeginInvoke(2000, CallbackMethod, null);
+            MessageBox.Show("Task BeginInvoke.");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var caller = new AsyncMethodCaller(AsyncMethod);
+            MessageBox.Show("Task Created.");
+            IAsyncResult result = caller.BeginInvoke(2000, null ,null);
+            MessageBox.Show("Task BeginInvoke.");
+            Thread.Sleep(100);
+
+            caller.EndInvoke(result);
+            MessageBox.Show("Task EndInvoke.");
+
         }
     }
 }
