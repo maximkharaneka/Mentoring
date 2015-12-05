5d74fb68bb6b8d3ad5fa8e04c41f272188ee23ad
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-12-05 20:51:22 +0300

    no message

---
+++ Mentoring/ClassMapper/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassMapper
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public class Mapper<TSource, TDestination>
+        {
+            Func<TSource, TDestination> mapFunction;
+            internal Mapper(Func<TSource, TDestination> func)
+            {
+                mapFunction = func;
+            }
+            public TDestination Map(TSource source)
+            {
+                return mapFunction(source);
+            }
+        }
+        public class MappingGenerator
+        {
+            public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
+            {
+                var sourceParam = Expression.Parameter(typeof(TSource));
+                var mapFunction =
+                    Expression.Lambda<Func<TSource, TDestination>>(
+                    Expression.New(typeof(TDestination)),
+                    sourceParam
+                    );
+
+                return new Mapper<TSource, TDestination>(mapFunction.Compile());
+            }
+        }
+        public class Foo { }
+        public class Bar { }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var mapGenerator = new MappingGenerator();
+            var mapper = mapGenerator.Generate<Foo, Bar>();
+
+            var res = mapper.Map(new Foo());
+        }
+    }
+}
