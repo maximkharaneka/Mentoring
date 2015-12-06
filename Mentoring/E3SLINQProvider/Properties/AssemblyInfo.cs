18f67dd0e22bf99370ced81c55ad1f15741f4a90
Maksim Kharaneka <maksim_kharaneka@epam.com>
2015-12-06 16:55:30 +0300

    e3s

--- Mentoring/E3SLINQProvider/Properties/AssemblyInfo.cs
+++ Mentoring/E3SLINQProvider/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("E3SLINQProvider")]
+[assembly: AssemblyTitle("Sample03")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("E3SLINQProvider")]
+[assembly: AssemblyProduct("Sample03")]
 [assembly: AssemblyCopyright("Copyright © EPAM Systems 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("73770751-11b9-4e76-b910-b819f2470f65")]
+[assembly: Guid("4b096486-7457-4911-8280-7ba7a2bb45a0")]
 
 // Version information for an assembly consists of the following four values:
 //
