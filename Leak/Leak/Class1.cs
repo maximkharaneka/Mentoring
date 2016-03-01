1dbf64d9b3ad9d8694f9279fe5a179364c2eccc7
unknown <Maksim_Kharaneka@EPBYGOMW0377.gomel.epam.com>
2016-03-01 21:02:30 +0300

    Profiling 1 task

---
+++ Leak/Leak/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leak
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Foo object1 = new Foo();
+            Foo object2 = new Foo();
+            // ...
+            object1.SomeEvent += object2.Bar;
+            // ...
+
+            // Should call this
+            // object1.SomeEvent -= object2.myEventHandler;
+
+            object2.Dispose();
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(1,2);
+        }
+    }
+}
---
+++ Leak/Leak/Foo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Leak
+{
+    class Foo
+    {
+        public event EventHandler<Action> BarEvent;
+
+        public void Baz()
+        {
+
+        }
+
+        public Foo()
+        {
+        }
+    }
+}
